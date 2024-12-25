07d039f161a86d5613c0751a2e12c668919f2c35
Kailuan Liu <kailuan1127@gmail.com>
2024-12-24 23:07:43 -0800

    milestone 3B

---
+++ bootcamp-milestone-2/src/app/home/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "./globals.css";
+
+export default function HomePage() {
+  return (
+    <main className={style.main}>
+        <div className={`${style.about} animate-slideFromLeft`}>
+            <div className={style.aboutImage}>
+              <img width="500" src="/Users/kailuanliu/IdeaProjects/y2023-24-q3-csc203-project4-liu-kailuan-huang-anna/images/Witch2.png" alt="witch for project"/>
+            </div>
+            <div className={style.aboutText}>
+                <p>My name is Kailuan Liu and currently studying computer science at Cal Poly SLO</p>
+            </div>
+        </div>
+    </main>
+  );
+}
