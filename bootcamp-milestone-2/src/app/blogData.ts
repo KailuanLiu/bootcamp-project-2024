5a0779ec581ae2c2588e7dd8687c83e4b54f5d5f
Kailuan Liu <kailuan1127@gmail.com>
2025-01-03 21:24:12 -0800

    update

--- bootcamp-milestone-2/src/app/blogData.ts
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -12,7 +12,7 @@ export const blogs: Blog[] = [
       title: "Blog1",
       date: "12/24/2024",
       description: "I finished my art 182 project3!",
-      image: "Project3.pdf",
+      image: "/Project3.pdf",
       imageAlt: "project-3",
       slug: "blog-1",
    },
@@ -20,7 +20,7 @@ export const blogs: Blog[] = [
       title: "Blog2",
       date: "12/25/2024",
       description: "It's Christmas! I ate some souffle pancakes",
-      image: "souffle_pancakes.jpg",
+      image: "/souffle_pancakes.jpg",
       imageAlt: "yummy souffle pancakes",
       slug: "blog-2"
    }
